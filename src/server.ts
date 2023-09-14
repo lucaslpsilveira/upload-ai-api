5ee4e5bb10da8fb9ae5adaf8485e1d7fac01ca50
lpedreira <lpedreira@grupopanvel.com.br>
2023-09-13 21:21:11 -0300

    Upload route

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import { fastify } from "fastify";
+import { prisma } from "./lib/prisma";
+import { getAllPromptsRoute } from "./routes/get-all-prompts";
+import { uploadoVideoRoute } from "./routes/upload-video";
 
  const app = fastify()
 
- app.get('/', () => {
-    return 'Hello World'
- })
+ app.register(getAllPromptsRoute)
+
+ app.register(uploadoVideoRoute)
 
  app.listen({
     port: 3333,
